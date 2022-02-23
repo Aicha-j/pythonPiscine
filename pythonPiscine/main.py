a22430c10dff5c379b4a7e42d3f813207caa3d4a
Aicha-j <jeledyaicha@gmail.com>
2022-02-23 16:13:25 +0100

    main

---
+++ pythonPiscine/main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 23 14:36:13 2022
+
+@author: aichajeledi
+"""
+import function as f
+
+
+#print(f.saisir())
+
+#print(f.afficheAge())
+
+#print(f.calculer())
+
+#print(f.paire_impaire())
+
+print(f.s_fibo()())
